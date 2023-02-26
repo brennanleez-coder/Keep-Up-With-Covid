879e5c204291001027697fa23fc6df872e870244
brennanleez-coder <brennanlee95@gmail.com>
2023-02-26 23:32:52 +0000

    Updates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <header class="text-center bg-blue-800 text-white p-4 mb-10" >
     <div class="text-3xl md:text-5xl font-bold mb-3">
       <i class="fa fa-viruses"></i>
-      COVID 19 tracker
+      Keep Up WIth Covid
     </div>
     <p>API by
       <a class="text-blue-300" href="https://covid19api.com" target="_blank">
